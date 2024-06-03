6d43aab80ec3271efe4163c8f674b3f3af3573c2
brownroyal <cabro1103@gmail.com>
2024-06-03 10:06:50 -0400

    Update 6/3
    Created ideas, meetings, minions js files to handle routing in a DRY way.
    Some dependencies are broken from npm install, that will be next on the agenda.

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,14 @@
-const checkMillionDollarIdea = () => {};
+//Make sure that any new or updated ideas are still worth one million
+const checkMillionDollarIdea = (req, res, next) => {
+    const numWeeks = req.body.numWeeks;
+    const weeklyRevenue = req.body.weeklyRevenue;
+    let value = numWeeks * weeklyRevenue;
+    if (value >= 1000000) {
+        res.send(value);
+    } else {
+        res.status(400).send(value);
+    };
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;

3642509931b065a5af0fc49dccc852c018a8e4b3
brownroyal <cabro1103@gmail.com>
2024-06-03 15:13:17 -0400

    test commit
    added a comment to line 17

--- server/api.js
+++ server/api.js
@@ -1,6 +1,17 @@
 const express = require('express');
+
+//Create parent router and child routers within.
 const apiRouter = express.Router();
+const minionRouter = require('./minions');
+const ideasRouter = require('./ideas');
+const meetingsRouter = require('./meetings');
 
+//Nest the routers.
+apiRouter.use('/minions', minionRouter);
+apiRouter.use('/ideas', ideasRouter);
+apiRouter.use('/meetings', meetingsRouter);
 
-
+//Export
 module.exports = apiRouter;
+
+//test comment for dev purposes
